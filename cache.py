f87266b7f9356f430614be3acd5e5352c2a2e7f2
MSD <dadamuhames@gmail.com>
2023-07-04 00:54:44 +0500

    second commit

--- cache.py
+++ cache.py
@@ -14,7 +14,8 @@ else:
     _cache = redis.Redis(
         host=os.environ.get('REDIS_HOST'),
         port=os.environ.get("REDIS_PORT"),
-        password=os.environ.get("REDIS_PASSWORD")
+        password=os.environ.get("REDIS_PASSWORD"),
+        ssl=True
     )
 
 
