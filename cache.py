ffedc784378258b44ea2480f38d2d93839dba06d
MSD <dadamuhames@gmail.com>
2023-07-04 00:48:03 +0500

    second commit

--- cache.py
+++ cache.py
@@ -14,13 +14,7 @@ else:
     _cache = redis.Redis(
         host=os.environ.get('REDIS_HOST'),
         port=os.environ.get("REDIS_PORT"),
-        # use your Redis user. More info https://redis.io/docs/management/security/acl/
-        username=os.environ.get('REDIS_USERNAME'),
-        password=os.environ.get("REDIS_PASSWORD"),  # use your Redis password
-        ssl=os.environ.get('REDIS_SSL'),
-        ssl_certfile="./redis_user.crt",
-        ssl_keyfile="./redis_user_private.key",
-        ssl_ca_certs="./redis_ca.pem",
+        password=os.environ.get("REDIS_PASSWORD")
     )
 
 
