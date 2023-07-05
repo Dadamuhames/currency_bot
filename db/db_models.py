7070539ad5003a76b445ad7f8e2ead0543c38c25
MSD <dadamuhames@gmail.com>
2023-07-05 15:23:16 +0500

    add columnists

--- db/db_models.py
+++ db/db_models.py
@@ -5,24 +5,24 @@ from .db_config import *
 class BaseChatModel:
     table = "" # database table name
 
-    def __init__(self):
-        self.mydb = mysql.connector.connect(
-            host=MYSQL_HOST,
-            user=MYSQL_USER,
-            password=MYSQL_PASSWORD,
-            database=MYSQL_DB
-        )
+    # def __init__(self):
+    #     self.mydb = mysql.connector.connect(
+    #         host=MYSQL_HOST,
+    #         user=MYSQL_USER,
+    #         password=MYSQL_PASSWORD,
+    #         database=MYSQL_DB
+    #     )
 
 
-        self.mycursor = self.mydb.cursor()
+    #     self.mycursor = self.mydb.cursor()
 
     # list items
     def list(self):
         sql = f"SELECT * FROM {self.table}"
-        self.mycursor.execute(sql)
-        queryset = self.mycursor.fetchall()
+        mycursor.execute(sql)
+        queryset = mycursor.fetchall()
 
-        self.mycursor.close()
+        mycursor.close()
         return queryset
     
 
@@ -30,11 +30,11 @@ class BaseChatModel:
     def single(self, chat_id):
         sql = f"""SELECT * FROM users WHERE chat_id={chat_id}"""
         
-        self.mycursor.execute(sql)
+        mycursor.execute(sql)
 
-        result = self.mycursor.fetchone()
+        result = mycursor.fetchone()
 
-        self.mycursor.close()
+        mycursor.close()
         return result
 
 
@@ -42,19 +42,19 @@ class BaseChatModel:
     def create(self, chat_id):
         sql = f"INSERT INTO {self.table} (chat_id) VALUES (%s)"
         val = (chat_id, )
-        self.mycursor.execute(sql, val)
-        self.mydb.commit()
-        self.mycursor.close()
+        mycursor.execute(sql, val)
+        mydb.commit()
+        mycursor.close()
 
 
 
     # delete item
     def delete_user(self, chat_id):
         sql = f"DELETE FROM {self.table} WHERE chat_id = {chat_id}"
-        self.mycursor.execute(sql)
+        mycursor.execute(sql)
 
-        self.mydb.commit()
-        self.mycursor.close()
+        mydb.commit()
+        mycursor.close()
 
 
 
@@ -66,39 +66,38 @@ class User(BaseChatModel):
     # update item
     def update_lang(self, chat_id, lang):
         sql = f"""UPDATE {self.table} SET language = %s WHERE chat_id = %s"""
-        self.mycursor.execute(sql, (lang, chat_id))
-
-        self.mydb.commit()
-        self.mycursor.close()
+        mycursor.execute(sql, (lang, chat_id))
+        mydb.commit()
+        mycursor.close()
 
 
 
     # update currencies
     def update_currencies(self, chat_id, currencies):
         sql = f"""UPDATE {self.table} SET currencies = %s WHERE chat_id = %s"""
-        self.mycursor.execute(sql, (currencies, chat_id))
+        mycursor.execute(sql, (currencies, chat_id))
 
-        self.mydb.commit()
-        self.mycursor.close()
+        mydb.commit()
+        mycursor.close()
 
 
     
     # change updates
     def change_updates(self, chat_id, updates):
         sql = f"""UPDATE {self.table} SET updates = %s WHERE chat_id = %s"""
-        self.mycursor.execute(sql, (updates, chat_id))
+        mycursor.execute(sql, (updates, chat_id))
 
-        self.mydb.commit()
-        self.mycursor.close()
+        mydb.commit()
+        mycursor.close()
 
 
 
     # get updates active
     def get_updatable_users(self):
         sql = f"SELECT chat_id FROM {self.table} WHERE updates = 1"
-        self.mycursor.execute(sql)
-        queryset = self.mycursor.fetchall()
-        self.mycursor.close()
+        mycursor.execute(sql)
+        queryset = mycursor.fetchall()
+        mycursor.close()
 
         return queryset
 
